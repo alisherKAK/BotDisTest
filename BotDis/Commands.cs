b636da0c440cba3d7140c71c664470c988024810
Alisher Kalabaev <alisher.alish333@gmail.com>
2021-04-24 00:22:44 +0600

    Initial Commit

---
+++ BotDis/Commands.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotDis
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Ping(string hello, int a)
+        {
+            await ReplyAsync("Pong " + hello + " " + a);
+        }
+    }
+}
---
+++ BotDis/Program.cs
@@ -0,0 +1,71 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotDis
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBot().GetAwaiter().GetResult();
+
+        private DiscordSocketClient client;
+        private CommandService commands;
+        private IServiceProvider services;
+
+        private async Task RunBot()
+        {
+            client = new DiscordSocketClient();
+            commands = new CommandService();
+            services = new ServiceCollection()
+                .AddSingleton(client)
+                .AddSingleton(commands)
+                .BuildServiceProvider();
+
+            string token = "ODM1MjA3MDE3NjEwMDg0Mzgy.YIMFJg.D7NyzyHQqb8U45D_k2xIMJtC3tg";
+
+            client.Log += ClientLog;
+
+            await RegisterCommands();
+
+            await client.LoginAsync(Discord.TokenType.Bot, token);
+
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task ClientLog(Discord.LogMessage arg)
+        {
+            Console.WriteLine(arg.Message);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommands()
+        {
+            client.MessageReceived += HandleMessage;
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+        }
+
+        private async Task HandleMessage(SocketMessage arg)
+        {
+            SocketUserMessage message = arg as SocketUserMessage;
+            SocketCommandContext context = new SocketCommandContext(client, message);
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            if(message.HasStringPrefix("!", ref argPos))
+            {
+                IResult result = await commands.ExecuteAsync(context, argPos, services);
+                if (!result.IsSuccess)
+                    Console.WriteLine(result.ErrorReason);
+            }
+
+        }
+    }
+}
